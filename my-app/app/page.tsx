912433707063c01b3cf8b5f2c5734528e34d2cdc
preeeetham <spreethamkumar5@gmail.com>
2024-12-25 21:48:48 +0530

    initial init

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import { WalletConnection } from "@/components/WalletConnection";
+
+function App(){
+    return (
+        <div className="flex flex-col items-end min-h-screen">
+            <WalletConnection />
+        </div>
+    )
+};
+
+export default App;
